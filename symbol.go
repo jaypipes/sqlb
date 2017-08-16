f0a1178d19d639e799f17655673c8cbdeafcc0ac
Jay Pipes <jaypipes@gmail.com>
2017-08-16 12:33:00 -0400

    Add missing symbol.go and scannable.go
    Forgot to git add a couple files

--- symbol.go
+++ symbol.go
@@ -5,4 +5,8 @@ var (
     SYM_AS_LEN = 4
     SYM_COMMA_WS = []byte(", ")
     SYM_COMMA_WS_LEN = 2
+    SYM_SELECT = []byte("SELECT ")
+    SYM_SELECT_LEN = 7
+    SYM_FROM = []byte(" FROM ")
+    SYM_FROM_LEN = 6
 )
