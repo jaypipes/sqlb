d0ecc6013352ccfbfdafcb7717950f3935e86d25
Jay Pipes <jaypipes@gmail.com>
2017-08-26 17:02:31 -0400

    Complete refactoring of select_test.go

--- select_test.go
+++ select_test.go
@@ -80,6 +80,92 @@ func TestSelectClause(t *testing.T) {
             },
             qs: "SELECT users.id, users.name FROM users",
         },
+        // Simple WHERE
+        selClauseTest{
+            c: &selectClause{
+                selections: []selection{users},
+                projected: &List{elements: []element{colUserName}},
+                where: &whereClause{
+                    filters: []*Expression{
+                        Equal(colUserName, "foo"),
+                    },
+                },
+            },
+            qs: "SELECT users.name FROM users WHERE users.name = ?",
+            qargs: []interface{}{"foo"},
+        },
+        // Simple LIMIT
+        selClauseTest{
+            c: &selectClause{
+                selections: []selection{users},
+                projected: &List{
+                    elements: []element{colUserName},
+                },
+                limit: &limitClause{limit: 10},
+            },
+            qs: "SELECT users.name FROM users LIMIT ?",
+            qargs: []interface{}{10},
+        },
+        // Simple ORDER BY
+        selClauseTest{
+            c: &selectClause{
+                selections: []selection{users},
+                projected: &List{
+                    elements: []element{colUserName},
+                },
+                orderBy: &OrderByClause{
+                    cols: &List{
+                        elements: []element{
+                            colUserName.Desc(),
+                        },
+                    },
+                },
+            },
+            qs: "SELECT users.name FROM users ORDER BY users.name DESC",
+        },
+        // Simple GROUP BY
+        selClauseTest{
+            c: &selectClause{
+                selections: []selection{users},
+                projected: &List{
+                    elements: []element{colUserName},
+                },
+                groupBy: &groupByClause{
+                    cols: &List{
+                        elements: []element{
+                            colUserName,
+                        },
+                    },
+                },
+            },
+            qs: "SELECT users.name FROM users GROUP BY users.name",
+        },
+        // GROUP BY, ORDER BY and LIMIT
+        selClauseTest{
+            c: &selectClause{
+                selections: []selection{users},
+                projected: &List{
+                    elements: []element{colUserName},
+                },
+                groupBy: &groupByClause{
+                    cols: &List{
+                        elements: []element{
+                            colUserName,
+                        },
+                    },
+                },
+                orderBy: &OrderByClause{
+                    cols: &List{
+                        elements: []element{
+                            colUserName.Desc(),
+                        },
+                    },
+                },
+                limit: &limitClause{limit: 10},
+            },
+            qs: "SELECT users.name FROM users GROUP BY users.name ORDER BY users.name DESC LIMIT ?",
+            qargs: []interface{}{10},
+        },
     }
     for _, test := range tests {
         expLen := len(test.qs)
@@ -95,135 +181,4 @@ func TestSelectClause(t *testing.T) {
         assert.Equal(written, s)
         assert.Equal(test.qs, string(b))
     }
-}
-
-func TestWhereSingleEqual(t *testing.T) {
-    assert := assert.New(t)
-
-    sel := Select(colUserName).Where(Equal(colUserName, "foo"))
-
-    exp := "SELECT users.name FROM users WHERE users.name = ?"
-    expLen := len(exp)
-    expArgCount := 1
-
-    assert.Equal(expLen, sel.size())
-    assert.Equal(expArgCount, sel.argCount())
-    assert.Equal(exp, sel.String())
-}
-
-func TestSelectLimit(t *testing.T) {
-    assert := assert.New(t)
-
-    sel := Select(colUserName).Limit(10)
-
-    exp := "SELECT users.name FROM users LIMIT ?"
-    expLen := len(exp)
-    expArgCount := 1
-
-    assert.Equal(expLen, sel.size())
-    assert.Equal(expArgCount, sel.argCount())
-    assert.Equal(exp, sel.String())
-}
-
-func TestSelectLimitWithOffset(t *testing.T) {
-    assert := assert.New(t)
-
-    sel := Select(colUserName).LimitWithOffset(10, 5)
-
-    exp := "SELECT users.name FROM users LIMIT ? OFFSET ?"
-    expLen := len(exp)
-    expArgCount := 2
-
-    assert.Equal(expLen, sel.size())
-    assert.Equal(expArgCount, sel.argCount())
-    assert.Equal(exp, sel.String())
-}
-
-func TestSelectOrderByAsc(t *testing.T) {
-    assert := assert.New(t)
-
-    sel := Select(colUserName).OrderBy(colUserName.Asc())
-
-    exp := "SELECT users.name FROM users ORDER BY users.name"
-    expLen := len(exp)
-    expArgCount := 0
-
-    assert.Equal(expLen, sel.size())
-    assert.Equal(expArgCount, sel.argCount())
-    assert.Equal(exp, sel.String())
-}
-
-func TestSelectOrderByMultiAscDesc(t *testing.T) {
-    assert := assert.New(t)
-
-    sel := Select(colUserName).OrderBy(colUserId.Asc(), colUserName.Desc())
-
-    exp := "SELECT users.name FROM users ORDER BY users.id, users.name DESC"
-    expLen := len(exp)
-    expArgCount := 0
-
-    assert.Equal(expLen, sel.size())
-    assert.Equal(expArgCount, sel.argCount())
-    assert.Equal(exp, sel.String())
-}
-
-func TestSelectStringArgs(t *testing.T) {
-    assert := assert.New(t)
-
-    sel := Select(colUserName).Where(In(colUserName, "foo", "bar"))
-
-    expStr := "SELECT users.name FROM users WHERE users.name IN (?, ?)"
-    expLen := len(expStr)
-    expArgCount := 2
-    expArgs := []interface{}{"foo", "bar"}
-
-    assert.Equal(expLen, sel.size())
-    assert.Equal(expArgCount, sel.argCount())
-
-    actStr, actArgs := sel.StringArgs()
-
-    assert.Equal(expStr, actStr)
-    assert.Equal(expArgs, actArgs)
-}
-
-func TestSelectGroupByAsc(t *testing.T) {
-    assert := assert.New(t)
-
-    sel := Select(colUserName).GroupBy(colUserName)
-
-    exp := "SELECT users.name FROM users GROUP BY users.name"
-    expLen := len(exp)
-    expArgCount := 0
-
-    assert.Equal(expLen, sel.size())
-    assert.Equal(expArgCount, sel.argCount())
-    assert.Equal(exp, sel.String())
-}
-
-func TestSelectGroupByMultiAsc(t *testing.T) {
-    assert := assert.New(t)
-
-    sel := Select(colUserName).GroupBy(colUserId, colUserName)
-
-    exp := "SELECT users.name FROM users GROUP BY users.id, users.name"
-    expLen := len(exp)
-    expArgCount := 0
-
-    assert.Equal(expLen, sel.size())
-    assert.Equal(expArgCount, sel.argCount())
-    assert.Equal(exp, sel.String())
-}
-
-func TestSelectGroupOrderLimit(t *testing.T) {
-    assert := assert.New(t)
-
-    sel := Select(colUserName).GroupBy(colUserName).OrderBy(colUserName.Desc()).Limit(10)
-
-    exp := "SELECT users.name FROM users GROUP BY users.name ORDER BY users.name DESC LIMIT ?"
-    expLen := len(exp)
-    expArgCount := 1
-
-    assert.Equal(expLen, sel.size())
-    assert.Equal(expArgCount, sel.argCount())
-    assert.Equal(exp, sel.String())
 }
