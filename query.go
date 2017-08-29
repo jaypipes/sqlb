f7785dafe7f8021fe74b645e2831a10f7d8bfa28
Jay Pipes <jaypipes@gmail.com>
2017-08-29 02:55:18 -0400

    Query struct now returned from sqlb.Select()
    Instead of returning a pointer to an unexported selectClause struct, we
    change the sqlb.Select() function to now return a new Query struct.

--- query.go
+++ query.go
@@ -36,3 +36,84 @@ func (q *Query) Where(e *Expression) *Query {
     q.sel.addWhere(e)
     return q
 }
+
+func Select(items ...element) *Query {
+    sel := &selectClause{
+        projected: &List{},
+    }
+
+    selectionMap := make(map[uint64]selection, 0)
+    projectionMap := make(map[uint64]projection, 0)
+
+    // For each scannable item we've received in the call, check what concrete
+    // type they are and, depending on which type they are, either add them to
+    // the returned selectClause's projected List or query the underlying
+    // table metadata to generate a list of all columns in that table.
+    for _, item := range items {
+        switch item.(type) {
+            case *joinClause:
+                j := item.(*joinClause)
+                if ! containsJoin(sel, j) {
+                    sel.joins = append(sel.joins, j)
+                    if _, ok := selectionMap[j.left.selectionId()]; ! ok {
+                        selectionMap[j.left.selectionId()] = j.left
+                        for _, proj := range j.left.projections() {
+                            projId := proj.projectionId()
+                            _, projExists := projectionMap[projId]
+                            if ! projExists {
+                                addToProjections(sel, proj)
+                                projectionMap[projId] = proj
+                            }
+                        }
+                    }
+                    if _, ok := selectionMap[j.right.selectionId()]; ! ok {
+                        for _, proj := range j.right.projections() {
+                            projId := proj.projectionId()
+                            _, projExists := projectionMap[projId]
+                            if ! projExists {
+                                addToProjections(sel, proj)
+                                projectionMap[projId] = proj
+                            }
+                        }
+                    }
+                }
+            case *Column:
+                v := item.(*Column)
+                sel.projected.elements = append(sel.projected.elements, v)
+                selectionMap[v.tbl.selectionId()] = v.tbl
+            case *List:
+                v := item.(*List)
+                for _, el := range v.elements {
+                    sel.projected.elements = append(sel.projected.elements, el)
+                    if isColumn(el) {
+                        c := el.(*Column)
+                        selectionMap[c.tbl.selectionId()] = c.tbl
+                    }
+                }
+            case *Table:
+                v := item.(*Table)
+                for _, cd := range v.tdef.projections() {
+                    addToProjections(sel, cd)
+                }
+                selectionMap[v.selectionId()] = v
+            case *TableDef:
+                v := item.(*TableDef)
+                for _, cd := range v.projections() {
+                    addToProjections(sel, cd)
+                }
+                selectionMap[v.selectionId()] = v
+            case *ColumnDef:
+                v := item.(*ColumnDef)
+                addToProjections(sel, v)
+                selectionMap[v.tdef.selectionId()] = v.tdef
+        }
+    }
+    selections := make([]selection, len(selectionMap))
+    x := 0
+    for _, sel := range selectionMap {
+        selections[x] = sel
+        x++
+    }
+    sel.selections = selections
+    return &Query{sel: sel}
+}
--- query_test.go
+++ query_test.go
@@ -6,14 +6,46 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
+type queryTest struct {
+    q *Query
+    qs string
+    qargs []interface{}
+}
+
 func TestQuery(t *testing.T) {
+    assert := assert.New(t)
+
+    m := testFixtureMeta()
+    users := m.Table("users")
+    colUserName := users.Column("name")
+
+    tests := []queryTest{
+        // Simple FROM
+        queryTest{
+            q: Select(users),
+            qs: "SELECT users.id, users.name FROM users",
+        },
+        // add WHERE
+        queryTest{
+            q: Select(users).Where(Equal(colUserName, "foo")),
+            qs: "SELECT users.id, users.name FROM users WHERE users.name = ?",
+            qargs: []interface{}{"foo"},
+        },
+    }
+    for _, test := range tests {
+        qs, qargs := test.q.StringArgs()
+        assert.Equal(len(test.qargs), len(qargs))
+        assert.Equal(test.qs, qs)
+    }
+}
+
+func TestModifyingQueryUpdatesBuffer(t *testing.T) {
     assert := assert.New(t)
 
     m := testFixtureMeta()
     users := m.TableDef("users")
 
-    sel := Select(users)
-    q := Query{sel: sel}
+    q := Select(users)
 
     qs, qargs := q.StringArgs()
     assert.Equal("SELECT users.id, users.name FROM users", qs)
