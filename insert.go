654d007975db99ebdfad6b57f33ec9a0fa2360d1
Jay Pipes <jaypipes@gmail.com>
2017-10-06 23:36:26 -0400

    add Insert() function
    Adds a new Insert() function that returns an InsertQuery struct.
    Similar to the SelectQuery struct, the InsertQuery struct implements
    the Query interface.
    
    The Insert() function accepts two arguments: a pointer to a Table
    struct that will be the target of the INSERT SQL statement and a map,
    keyed by string names of columns, of the values to use in the VALUES
    clause of the INSERT statement.
    
    Issue #67

---
+++ insert.go
@@ -0,0 +1,81 @@
+package sqlb
+
+import (
+    "errors"
+)
+
+var (
+    ERR_INSERT_NO_VALUES = errors.New("No values supplied.")
+    ERR_INSERT_UNKNOWN_COLUMN = errors.New("Received an unknown column.")
+)
+
+type InsertQuery struct {
+    e error
+    b []byte
+    args []interface{}
+    stmt *insertStatement
+}
+
+func (q *InsertQuery) IsValid() bool {
+    return q.e == nil &&  q.stmt != nil
+}
+
+func (q *InsertQuery) Error() error {
+    return q.e
+}
+
+func (q *InsertQuery) String() string {
+    size := q.stmt.size()
+    argc := q.stmt.argCount()
+    if len(q.args) != argc  {
+        q.args = make([]interface{}, argc)
+    }
+    if len(q.b) != size {
+        q.b = make([]byte, size)
+    }
+    q.stmt.scan(q.b, q.args)
+    return string(q.b)
+}
+
+func (q *InsertQuery) StringArgs() (string, []interface{}) {
+    size := q.stmt.size()
+    argc := q.stmt.argCount()
+    if len(q.args) != argc  {
+        q.args = make([]interface{}, argc)
+    }
+    if len(q.b) != size {
+        q.b = make([]byte, size)
+    }
+    q.stmt.scan(q.b, q.args)
+    return string(q.b), q.args
+}
+
+// Given a table and a map of column name to value for that column to insert,
+// returns an InsertQuery that will produce an INSERT SQL statement 
+func Insert(t *Table, values map[string]interface{}) *InsertQuery {
+    if len(values) == 0 {
+        return &InsertQuery{e: ERR_INSERT_NO_VALUES}
+    }
+
+    // Make sure all keys in the map point to actual columns in the target
+    // table.
+    cols := make([]*Column, len(values))
+    vals := make([]interface{}, len(values))
+    x := 0
+    for k, v := range values {
+        c := t.C(k)
+        if c == nil {
+            return &InsertQuery{e: ERR_INSERT_UNKNOWN_COLUMN}
+        }
+        cols[x] = c
+        vals[x] = v
+        x++
+    }
+
+    stmt := &insertStatement{
+        table: t,
+        columns: cols,
+        values: vals,
+    }
+    return &InsertQuery{stmt: stmt}
+}
---
+++ insert_test.go
@@ -0,0 +1,53 @@
+package sqlb
+
+import (
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+)
+
+func TestInsertQuery(t *testing.T) {
+    assert := assert.New(t)
+
+    m := testFixtureMeta()
+    users := m.Table("users")
+
+    tests := []struct{
+        name string
+        q *InsertQuery
+        qs string
+        qargs []interface{}
+        qe error
+    }{
+        {
+            name: "Values missing",
+            q: Insert(users, nil),
+            qe: ERR_INSERT_NO_VALUES,
+        },
+        {
+            name: "Unknown column",
+            q: Insert(users, map[string]interface{}{"unknown": 1}),
+            qe: ERR_INSERT_UNKNOWN_COLUMN,
+        },
+        {
+            name: "Simple INSERT",
+            q: Insert(users, map[string]interface{}{"id": 1}),
+            qs: "INSERT INTO users (id) VALUES (?)",
+            qargs: []interface{}{1},
+        },
+    }
+    for _, test := range tests {
+        if test.qe != nil {
+            assert.Equal(test.qe, test.q.Error())
+            continue
+        } else if test.q.Error() != nil {
+            qe := test.q.Error()
+            assert.Fail(qe.Error())
+            continue
+        }
+        qs, qargs := test.q.StringArgs()
+        assert.Equal(len(test.qargs), len(qargs))
+        assert.Equal(test.qs, qs)
+    }
+}
+
