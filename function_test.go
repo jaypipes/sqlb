b88e15d88266f20be1402fce6321edecfd382755
Jay Pipes <jaypipes@gmail.com>
2017-11-10 10:35:09 -0500

    Create dialect-specific tests for functions
    Since SQL functions are one of the first places where SQL dialects rear
    their ugly head with regards to slight differences between the output
    SQL string, this patch gets ready for PostgreSQL-specific dialect
    changes in functions by making the test struct contain a map of Dialect
    to expected SQL string output.
    
    Issue #56

--- function_test.go
+++ function_test.go
@@ -16,114 +16,156 @@ func TestFunctions(t *testing.T) {
 	tests := []struct {
 		name  string
 		c     *sqlFunc
-		qs    string
+		qs    map[Dialect]string
 		qargs []interface{}
 	}{
 		{
 			name: "MAX(column)",
 			c:    Max(colUserName),
-			qs:   "MAX(users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "MAX(users.name)",
+			},
 		},
 		{
 			name: "aliased function",
 			c:    Max(colUserName).As("max_name"),
-			qs:   "MAX(users.name) AS max_name",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "MAX(users.name) AS max_name",
+			},
 		},
 		{
 			name: "MIN(column)",
 			c:    Min(colUserName),
-			qs:   "MIN(users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "MIN(users.name)",
+			},
 		},
 		{
 			name: "SUM(column)",
 			c:    Sum(colUserName),
-			qs:   "SUM(users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "SUM(users.name)",
+			},
 		},
 		{
 			name: "AVG(column)",
 			c:    Avg(colUserName),
-			qs:   "AVG(users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "AVG(users.name)",
+			},
 		},
 		{
 			name: "COUNT(*)",
 			c:    Count(users),
-			qs:   "COUNT(*)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "COUNT(*)",
+			},
 		},
 		{
 			name: "COUNT(DISTINCT column)",
 			c:    CountDistinct(colUserName),
-			qs:   "COUNT(DISTINCT users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "COUNT(DISTINCT users.name)",
+			},
 		},
 		{
 			name: "Ensure AS alias not in COUNT(DISTINCT column)",
 			c:    CountDistinct(colUserName.As("user_name")),
-			qs:   "COUNT(DISTINCT users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "COUNT(DISTINCT users.name)",
+			},
 		},
 		{
 			name: "CAST(column AS type)",
 			c:    Cast(colUserName, SQL_TYPE_TEXT),
-			qs:   "CAST(users.name AS TEXT)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "CAST(users.name AS TEXT)",
+			},
 		},
 		{
 			name: "TRIM(column)",
 			c:    Trim(colUserName),
-			qs:   "TRIM(users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "TRIM(users.name)",
+			},
 		},
 		{
 			name: "CHAR_LENGTH(column)",
 			c:    CharLength(colUserName),
-			qs:   "CHAR_LENGTH(users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "CHAR_LENGTH(users.name)",
+			},
 		},
 		{
 			name: "BIT_LENGTH(column)",
 			c:    BitLength(colUserName),
-			qs:   "BIT_LENGTH(users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "BIT_LENGTH(users.name)",
+			},
 		},
 		{
 			name: "ASCII(column)",
 			c:    Ascii(colUserName),
-			qs:   "ASCII(users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "ASCII(users.name)",
+			},
 		},
 		{
 			name: "REVERSE(column)",
 			c:    Reverse(colUserName),
-			qs:   "REVERSE(users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "REVERSE(users.name)",
+			},
 		},
 		{
 			name: "CONCAT(column, column)",
 			c:    Concat(colUserName, colUserName),
-			qs:   "CONCAT(users.name, users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "CONCAT(users.name, users.name)",
+			},
 		},
 		{
-			name:  "CONCAT_WS(string, column, column)",
-			c:     ConcatWs("-", colUserName, colUserName),
-			qs:    "CONCAT_WS(?, users.name, users.name)",
+			name: "CONCAT_WS(string, column, column)",
+			c:    ConcatWs("-", colUserName, colUserName),
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "CONCAT_WS(?, users.name, users.name)",
+			},
 			qargs: []interface{}{"-"},
 		},
 		{
 			name: "NOW()",
 			c:    Now(),
-			qs:   "NOW()",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "NOW()",
+			},
 		},
 		{
 			name: "CURRENT_TIMESTAMP()",
 			c:    CurrentTimestamp(),
-			qs:   "CURRENT_TIMESTAMP()",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "CURRENT_TIMESTAMP()",
+			},
 		},
 		{
 			name: "CURRENT_TIME()",
 			c:    CurrentTime(),
-			qs:   "CURRENT_TIME()",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "CURRENT_TIME()",
+			},
 		},
 		{
 			name: "CURRENT_DATE()",
 			c:    CurrentDate(),
-			qs:   "CURRENT_DATE()",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "CURRENT_DATE()",
+			},
 		},
 		{
 			name: "EXTRACT(unit FROM column)",
 			c:    Extract(colUserName, UNIT_MINUTE_SECOND),
-			qs:   "EXTRACT(MINUTE_SECOND FROM users.name)",
+			qs: map[Dialect]string{
+				DIALECT_MYSQL: "EXTRACT(MINUTE_SECOND FROM users.name)",
+			},
 		},
 	}
 	for _, test := range tests {
@@ -131,16 +173,20 @@ func TestFunctions(t *testing.T) {
 		argc := test.c.argCount()
 		assert.Equal(expArgc, argc)
 
-		expLen := len(test.qs)
-		size := test.c.size()
-		size += interpolationLength(DIALECT_MYSQL, argc)
-		assert.Equal(expLen, size)
+		// Test the MySQL SQL dialect output
+		{
+			qs := test.qs[DIALECT_MYSQL]
+			expLen := len(qs)
+			size := test.c.size()
+			size += interpolationLength(DIALECT_MYSQL, argc)
+			assert.Equal(expLen, size)
 
-		b := make([]byte, size)
-		curArg := 0
-		written := test.c.scan(b, test.qargs, &curArg)
+			b := make([]byte, size)
+			curArg := 0
+			written := test.c.scan(b, test.qargs, &curArg)
 
-		assert.Equal(written, size)
-		assert.Equal(test.qs, string(b))
+			assert.Equal(written, size)
+			assert.Equal(qs, string(b))
+		}
 	}
 }
