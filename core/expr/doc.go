03ac9cc445f115fa92bffe96d9fe2be1eea1831f
Jay Pipes <jaypipes@gmail.com>
2024-09-20 08:47:20 -0400

    Add package doc comment for core/expr
    Signed-off-by: Jay Pipes <jaypipes@gmail.com>

---
+++ core/expr/doc.go
@@ -0,0 +1,10 @@
+//
+// Use and distribution licensed under the Apache license version 2.
+//
+// See the COPYING file in the root project directory for full text.
+//
+
+// Package expr contains functions that produce sqlb structs that can be
+// chained together to form SQL statements and ultimately passed to sqlb's
+// [sqlb.Query] and [sqlb.QueryContext] functions.
+package expr
