6a7c6cbf4fb0a1c00375cd4e425cfdaa9b82bd54
Jay Pipes <jaypipes@gmail.com>
2024-09-20 08:38:59 -0400

    Add docs for core/reflect package
    Signed-off-by: Jay Pipes <jaypipes@gmail.com>

---
+++ core/reflect/doc.go
@@ -0,0 +1,10 @@
+//
+// Use and distribution licensed under the Apache license version 2.
+//
+// See the COPYING file in the root project directory for full text.
+//
+
+// Package reflect contains utility functions that allow sqlb to collect
+// database metadata and construct a set of sqlb [core/meta] structs that
+// describe the database's tables and views.
+package reflect
