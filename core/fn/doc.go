915e6eb35b5320d830a37023a91acfaa549afa7e
Jay Pipes <jaypipes@gmail.com>
2024-09-20 08:43:47 -0400

    Add package doc comment for core/fn
    Signed-off-by: Jay Pipes <jaypipes@gmail.com>

---
+++ core/fn/doc.go
@@ -0,0 +1,9 @@
+//
+// Use and distribution licensed under the Apache license version 2.
+//
+// See the COPYING file in the root project directory for full text.
+//
+
+// Package fn contains functions that return structs that represent SQL
+// functions
+package fn
